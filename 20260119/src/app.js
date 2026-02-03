30f72feb4eb11a7ea2f95d09aa1d1a1fe0793aa4
2303A52158 <2303A52158@sru.edu.in>
2026-02-03 18:50:52 +0530

    Added 20260119

---
+++ 20260119/src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+const errorHandler = require("./middleware/errorHandler");
+
+const courseRoutes = require("./routes/course.routes");
+const enrollmentRoutes = require("./routes/enrollment.routes");
+
+const app = express();
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "public")));
+
+
+//app.get("/", (req, res) => res.json({ message: "Training Platform API running" }));
+
+app.use("/courses", courseRoutes);
+app.use("/enrollments", enrollmentRoutes);
+
+app.use(errorHandler);
+
+module.exports = app;
